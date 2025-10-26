ad79e52e1a5534a217b60f432968ce346f464823
Isham's Mac Mini <ishamsmacmini@Omars-Mac-mini.local>
2025-10-26 21:22:24 +0600

    Some changes

--- productivephone.py
+++ productivephone.py
@@ -7,12 +7,17 @@ print("/" * 10 + " Starting " + "/" * 10)
 # Step 1: Import libraries
 import pyaudio
 import wave
-import tkinter as tk
-import serial
-import pygame
 import subprocess
 import random
 import pyttsx3
 import webbrowser
 import openai
 from dotenv import load_dotenv
+
+# Step 2: Define functions
+def CallApp(name, number):
+    _my_list = []
+    _my_list.append(name)
+    _my_list.append(number)
+    print(_my_list)
+    
