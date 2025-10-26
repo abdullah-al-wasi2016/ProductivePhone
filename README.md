b210e4743955816c625db1610797c0779ddb947a
Isham's Mac Mini <ishamsmacmini@Omars-Mac-mini.local>
2025-10-26 21:14:18 +0600

    first Mobile

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is my ProductivePhone, the work mainly starts at August 2025.
+# Copyrights: This project is a personal project, so no one can edit it.
---
+++ productivephone.py
@@ -0,0 +1,18 @@
+"""This is the ProductivePhone, the large project which started at August 2025.
+This has more improved code, as this is using Python 3.
+"""
+
+print("/" * 10 + " Starting " + "/" * 10)
+
+# Step 1: Import libraries
+import pyaudio
+import wave
+import tkinter as tk
+import serial
+import pygame
+import subprocess
+import random
+import pyttsx3
+import webbrowser
+import openai
+from dotenv import load_dotenv
